a281ca1c0b83e29db330d6d821459c72af61e409
venkey <venkey@gmail.com>
2023-04-13 09:10:52 +0530

    Push the code Git Rep

---
+++ venki/src/venki/mabhu.java
@@ -0,0 +1,19 @@
+package venki;
+	import java.lang.*;
+	import java.io.*;
+	import java.util.*;
+	 
+	public class mabhu {
+	
+	    public static void main(String[] args)
+	    {
+	        String input = "GeeksForGeeks";
+	        char[] try1 = input.toCharArray();
+	        for (int i = try1.length - 1; i >= 0; i--)
+	            System.out.print(try1[i]);
+	    }
+	
+
+	}
+
+
